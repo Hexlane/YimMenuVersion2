b63c47c10c5cf11a7b89642a1e14554f1fe8f199
𝐶𝑆𝑌𝑂𝑁 <134310111+CSY0N@users.noreply.github.com>
2025-12-26 23:55:27 +0100

    Update AllBunkerUnlocksOnly.cpp

--- src/game/features/recovery/unlocks/AllBunkerUnlocksOnly.cpp
+++ src/game/features/recovery/unlocks/AllBunkerUnlocksOnly.cpp
@@ -5,29 +5,36 @@
 
 namespace YimMenu::Features
 {
-	class BunkerUnlocks : public Command
-	{
-		using Command::Command;
-		virtual void OnCall() override
-		{
-			if (*Pointers.IsSessionStarted)
-			{
-				Stats::SetPackedBool(15381, 15382, 1);
-				Stats::SetPackedBool(15428, 15439, 1);
-				Stats::SetPackedBool(15447, 15474, 1);
-				Stats::SetPackedBool(15491, 15499, 1);
-				STATS::STAT_SAVE(0, 0, 3, 0);
-			}
-			else
-			{
-				Notifications::Show("All Bunker research unlocked!", "All Bunker Research Only Please join any freemode session.", NotificationType::Error);
-			}
-		}
-	};
-	static BunkerUnlocks _BunkerUnlocks{
-	    "BunkerUnlocks",
-	    "Bunker Unlocks",
-	    "Bunker Unlocks"};
+    class BunkerUnlocks : public Command
+    {
+        using Command::Command;
+        virtual void OnCall() override    
+        {
+            if (*Pointers.IsSessionStarted)
+            {
+                for (int i = 15381; i <= 15382; ++i)
+                    Stats::SetPackedBool(i, true);
+
+                for (int i = 15428; i <= 15439; ++i)
+                    Stats::SetPackedBool(i, true);
+
+                for (int i = 15447; i <= 15474; ++i)
+                    Stats::SetPackedBool(i, true);
+
+                for (int i = 15491; i <= 15499; ++i)
+                    Stats::SetPackedBool(i, true);
+
+                STATS::STAT_SAVE(0, 0, 3, 0);
+            }
+            else
+            {
+                Notifications::Show("All Bunker research unlocked!", "All Bunker Research Only Please join any freemode session.", NotificationType::Error);
+            }
+        }
+    };
+    static BunkerUnlocks _BunkerUnlocks{
+        "BunkerUnlocks",
+        "Bunker Unlocks",
+        "Bunker Unlocks"};
 
 }
-
